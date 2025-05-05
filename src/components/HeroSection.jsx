c0ed19bd2b88e64e7f3bd0039dc064befb50aaf6
ansh <ansh@Anshs-MacBook-Pro-4.local>
2025-05-05 17:06:58 +0530

    node_modules deleted

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,27 +1,27 @@
 import IncentricCircle from "./IncenctricCircle";
+import { useNavigate } from "react-router-dom";
+
 function HeroSection() {
+  const navigate = useNavigate();
   return (
     <div className="container">
       <div className="title-logo">
-      <div>
-        <h1 style={{fontSize:'4vw',color: 'rgb(64, 64, 153)'}}>Startup Valuation <br/>Made Simple</h1>
-        <p style={{fontSize:'1.5vw',color: 'rgb(64, 64, 153)'}}>
-          Understand, explore, and calculate startup valuations with our<br />
-          comprehensive suite of tools. From seed stage to Series C and beyond,<br />
-          we help founders and investors make sense of the numbers.
-        </p>
-        <div className="buttons">
-        <button>Calculate Your Valuation</button>
-        <button>Explore Valuation Concepts</button>
+        <div>
+          <h1>Startup Valuation <br />Made Simple</h1>
+          <p>
+            Understand, explore, and calculate startup valuations with our<br />
+            comprehensive suite of tools. From seed stage to Series C and beyond,<br />
+            we help founders and investors make sense of the numbers.
+          </p>
+          <div className="buttons">
+            <button onClick={() => navigate('/Calculate')}>Calculate Your Valuation</button>
+            <button onClick={() => navigate('/Learn')}>Explore Valuation Concepts</button>
+          </div>
         </div>
+        <div><IncentricCircle /></div>
       </div>
-      
-      <div><IncentricCircle/></div>
-
-      </div>
-
-    
     </div>
   );
 }
-export default HeroSection
+
+export default HeroSection;
